f0dc6d6c1a453718e042ba036764240bc41f3fbb
Tomas Bjerre <tomas.bjerre85@gmail.com>
2023-06-11 14:27:26 +0200

    chore: build-docker.sh

---
+++ build-docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source ~/.bashrc
+source gradle.properties
+
+npx runnable-jar-to-docker@latest \
+ --docker-username tomasbjerre \
+ --docker-password $dockerhub_token \
+ --maven-group $group \
+ --maven-artifact ${PWD##*/}  \
+ --maven-version $(npx git-changelog-command-line --print-highest-version)
