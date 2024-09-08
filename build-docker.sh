17defeff2f95a6f5c6b947b165af27a888866d2b
Tomas Bjerre <tomas.bjerre85@gmail.com>
2024-09-08 08:28:27 +0200

    chore: docker script from local repo

--- build-docker.sh
+++ build-docker.sh
@@ -8,4 +8,5 @@ npx runnable-jar-to-docker@latest \
  --docker-password $dockerhub_token \
  --maven-group $group \
  --maven-artifact ${PWD##*/}  \
- --maven-version $(npx git-changelog-command-line --print-highest-version)
+ --maven-version $(npx git-changelog-command-line --print-highest-version) \
+ --repository-url "file://$HOME/.m2/repository"
